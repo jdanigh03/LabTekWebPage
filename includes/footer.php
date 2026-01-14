66bff33285bcae2c8c563c5acfabe7671a458e65
jdanigh03 <danielguevarahoyos03@gmail.com>
2026-01-13 21:36:07 -0400

    feat/webpage-structure

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+    </main>
+    <footer>
+        <div class="container">
+            <p>&copy; <?php echo date('Y'); ?> LABTEK. Todos los derechos reservados.</p>
+        </div>
+    </footer>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
